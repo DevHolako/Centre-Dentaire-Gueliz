d98e5f3c4c620b38e1993413b8256ca7a138a96e
DevHolako <holakonoob@gmail.com>
2023-06-02 12:06:09 +0100

    admin routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
+import Admin from "@/pages/admin";
 import Login from "@/pages/auth/Login";
 import Layout from "@/pages/layouts/Layout";
 import ReceptionistDashbord from "@/pages/receptionist";
+import Patients from "@/pages/receptionist/Patients";
+import Settings from "@/pages/receptionist/Profile";
 import DocPatientsList, {
   GetPatientByDoc,
 } from "@/pages/receptionist/DocPatientsList";
-import Patients from "@/pages/receptionist/Patients";
-import Settings from "@/pages/receptionist/Profile";
 import { createBrowserRouter } from "react-router-dom";
 
 //**end of imports */
@@ -34,6 +35,20 @@ export const router = createBrowserRouter([
         path: "doc/:id",
         element: <DocPatientsList />,
         loader: GetPatientByDoc,
+      },
+    ],
+  },
+  {
+    path: "admin",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Admin />,
+      },
+      {
+        path: "setting",
+        element: <Settings />,
       },
     ],
   },
